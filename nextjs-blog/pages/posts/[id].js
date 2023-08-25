bb6ffaad7a02db8d7a2c7693770c3c8dce55440c
Matheus <math.av7@outlook.com>
2023-08-24 21:24:11 -0300

    feature: using Dynamic Routes

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,5 +1,8 @@
+import Date from "../../components/date";
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
+import utilStyles from "../../styles/utils.module.css";
 
 // 1° faz a busca de todos os IDs possíves e retorn em { paths: { params: { id: ""}}}
 export async function getStaticPaths() {
@@ -10,10 +13,12 @@ export async function getStaticPaths() {
   };
 }
 
+
 // 2° faz a busca de todas as propriedades estaticas com base nos IDs retornados no passo 1
 // e retorna
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
+  console.log(postData);
   return {
     props: {
       postData,
@@ -21,16 +26,20 @@ export async function getStaticProps({ params }) {
   };
 }
 
+
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
+      </article>
     </Layout>
   );
 }
